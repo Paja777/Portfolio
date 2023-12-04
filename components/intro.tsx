85720809255320544b7d0ca680510a184235e24f
Paja777 <pavle.rvovic95@gmail.com>
2023-12-04 22:03:10 +0100

    design

--- components/intro.tsx
+++ components/intro.tsx
@@ -11,7 +11,7 @@ import image1 from "@/public/IMG_3010.jpg";
 
 export default function Intro() {
   return (
-    <section className="mb-28 max-w-[50rem] rext-center sm:mb-0">
+    <section className="mb-28 max-w-[50rem] rext-center sm:mb-0 scroll-mt-28" id="home">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
